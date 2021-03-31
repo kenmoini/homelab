e7560040c4614475974143887a62bba8ca7acd83
Ken Moini <ken@kenmoini.com>
2021-03-30 22:02:51 -0400

    add nfs caas

---
+++ containers-as-a-service/caas-nfs/config/service_start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -x
+
+source /opt/service-containers/caas-nfs/config/service_vars.sh
+
+/opt/service-containers/caas-nfs/config/service_stop.sh
+
+sleep 3
+
+echo "Checking for stale network lock file..."
+FILE_CHECK="/var/lib/cni/networks/${NETWORK_NAME}/${IP_ADDRESS}"
+if [[ -f "$FILE_CHECK" ]]; then
+    rm $FILE_CHECK
+fi
+
+echo "Starting container ${CONTAINER_NAME}..."
+/usr/bin/podman run -d --name "${CONTAINER_NAME}" \
+ --network "${NETWORK_NAME}" --ip "${IP_ADDRESS}" \
+ $CONTAINER_PORTS \
+ -v ${EXPORTS_CFG_VOLUME_MOUNT} \
+ -v ${NFS_BASE_VOLUME_MOUNT} \
+ ${RESOURCE_LIMITS} \
+ ${CONTAINER_SOURCE}
---
+++ containers-as-a-service/caas-nfs/config/service_vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+CONTAINER_NAME="caas-nfs"
+NETWORK_NAME="lanBridge"
+IP_ADDRESS="192.168.42.31"
+
+CONTAINER_PORTS="-p 2049/tcp -p 111/tcp -p 32765/tcp -p 32767/tcp"
+
+EXPORTS_CFG_VOLUME_MOUNT="/opt/service-containers/${CONTAINER_NAME}/volumes/etc-conf/exports:/etc/exports"
+NFS_BASE_VOLUME_MOUNT="/mnt/nvme_7TB/nfs:/nfs"
+
+CONTAINER_SOURCE="quay.io/kenmoini/nfs-ubi:latest"
+
+RESOURCE_LIMITS="-m 512m"
