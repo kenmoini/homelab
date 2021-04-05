8aa1a380744458b0198d6ff579d91d9e45d8fe32
Ken Moini <ken@kenmoini.com>
2021-04-04 20:27:44 -0400

    fix nfs autoprovisioner

--- openshift-quickstarts/cluster-init/nfs-storageclass/run.sh
+++ openshift-quickstarts/cluster-init/nfs-storageclass/run.sh
@@ -12,9 +12,15 @@ oc new-project $NAMESPACE_NFS
 echo "Applying RBAC..."
 oc apply -f deploy/rbac.yaml
 
+sleep 3
+
 echo "Applying security constraints..."
 oc adm policy add-role-to-user use-scc-hostmount-anyuid system:serviceaccount:$NAMESPACE_NFS:nfs-client-provisioner
 oc create role use-scc-hostmount-anyuid --verb=use --resource=scc --resource-name=hostmount-anyuid -n $NAMESPACE_NFS
+oc adm policy add-role-to-user use-scc-hostmount-anyuid system:serviceaccount:$NAMESPACE_NFS:nfs-client-provisioner
+oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:$NAMESPACE_NFS:nfs-client-provisioner
+
+sleep 3
 
 echo "Applying manifests..."
 oc apply -f deploy/deployment.yaml
