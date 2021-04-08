391eaa6a763b4929f3cc1694cb843e2731e6af78
Ken Moini <ken@kenmoini.com>
2021-04-08 01:58:27 -0400

    finish ldap run script

--- openshift-quickstarts/cluster-init/idp-ldap/run.sh
+++ openshift-quickstarts/cluster-init/idp-ldap/run.sh
@@ -9,3 +9,6 @@ oc create configmap labdap-ca --from-file=ca.crt=cert.pem -n openshift-config
 sed "s/SOME_SECURE_PASSWORD/${BIND_PASSWORD}/g" ldap-sync.yaml.template > lab-sync.yaml
 
 oc adm groups sync --sync-config=ldap-sync.yaml --confirm
+
+oc adm policy add-cluster-role-to-group cluster-admin admins
+oc adm policy add-cluster-role-to-group cluster-admin labadmins
