a01994b87cbb80dd15544b77cfe8af5f1eddb9ed
Ken Moini <ken@kenmoini.com>
2021-04-13 15:46:52 -0400

    fix up ldap idp

--- openshift-quickstarts/cluster-init/idp-ldap/run.sh
+++ openshift-quickstarts/cluster-init/idp-ldap/run.sh
@@ -2,11 +2,13 @@
 
 BIND_PASSWORD=$1
 
-oc create secret generic labdap-bind-password --from-literal=bindPassword=${BIND_PASSWORD} -n openshift-config
+oc create secret generic ldap-bind-password --from-literal=bindPassword=${BIND_PASSWORD} -n openshift-config
 
-oc create configmap labdap-ca --from-file=ca.crt=cert.pem -n openshift-config
+oc create configmap ldap-ca --from-file=ca.crt=cert.pem -n openshift-config
 
-sed "s/SOME_SECURE_PASSWORD/${BIND_PASSWORD}/g" ldap-sync.yaml.template > lab-sync.yaml
+oc apply -f ldap-idp.yaml
+
+sed "s/SOME_SECURE_PASSWORD/${BIND_PASSWORD}/g" ldap-sync.yaml.template > ldap-sync.yaml
 
 oc adm groups sync --sync-config=ldap-sync.yaml --confirm
 
