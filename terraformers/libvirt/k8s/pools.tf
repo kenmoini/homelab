f8c0ce98c9d9621530be7dc4e003297e7db4cb87
Ken Moini <ken@kenmoini.com>
2021-04-17 14:48:37 -0400

    update k8s tf

--- terraformers/libvirt/k8s/pools.tf
+++ terraformers/libvirt/k8s/pools.tf
@@ -1,18 +1,21 @@
 # Uncomment these resources if you need to create the pools
 
 #resource "libvirt_pool" "serenity" {
+#  provider = libvirt.serenity
 #  name = "serenity"
 #  type = "dir"
 #  path = "/mnt/nfs/vms/serenity"
 #}
 #
 #resource "libvirt_pool" "rocinante" {
+#  provider = libvirt.rocinante
 #  name = "rocinante"
 #  type = "dir"
 #  path = "/mnt/nfs/vms/rocinante"
 #}
 #
 #resource "libvirt_pool" "raza" {
+#  provider = libvirt.raza
 #  name = "raza"
 #  type = "dir"
 #  path = "/mnt/nvme_7TB/nfs/vms/raza"
