64ff46a1a167b4db75617e2605f5c97911e1d17d
Ken Moini <ken@kenmoini.com>
2021-06-16 10:05:23 -0400

    set upstream override for core-dns to piholes

--- containers-as-a-service/dns-core-1/config/service_vars.sh
+++ containers-as-a-service/dns-core-1/config/service_vars.sh
@@ -6,6 +6,8 @@ IP_ADDRESS="192.168.42.9"
 CONTAINER_PORT="53"
 
 VOLUME_MOUNT_ONE="/opt/service-containers/${CONTAINER_NAME}/volumes/etc-conf:/etc/go-zones/"
+VOLUME_MOUNT_TWO="/opt/service-containers/${CONTAINER_NAME}/volumes/vendor-conf:/opt/app-root/vendor/bind/"
+
 CONTAINER_SOURCE="quay.io/kenmoini/go-zones:file-to-bind"
 
-RESOURCE_LIMITS="-m 512m"
+RESOURCE_LIMITS="-m 1024m"
