c5c491148c9af3842dbe384c9e6d37714fdd0b1e
Ken Moini <ken@kenmoini.com>
2021-08-16 09:33:15 -0400

    fix up ocp stuff

--- openshift-quickstarts/cluster-init/nfs-storageclass/run.sh
+++ openshift-quickstarts/cluster-init/nfs-storageclass/run.sh
@@ -10,24 +10,26 @@ echo "*******************************************"
 echo "Creating project..."
 oc new-project $NAMESPACE_NFS
 
-echo "Applying RBAC..."
-oc apply -f deploy/rbac.yaml
+#echo "Applying RBAC..."
+#oc apply -f deploy/rbac.yaml
+echo "Applying manifests..."
+oc process -f template.yaml --param NFS_SERVER="${NFS_SERVER}" --param NFS_PATH="${NFS_PATH}" | oc create -f -
 
 sleep 3
 
 echo "Applying security constraints..."
+oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:$NAMESPACE_NFS:nfs-client-provisioner
+oc adm policy add-scc-to-user hostmount-anyuid -n ${NAMESPACE_NFS} -z nfs-client-provisioner
 oc adm policy add-role-to-user use-scc-hostmount-anyuid system:serviceaccount:$NAMESPACE_NFS:nfs-client-provisioner
 oc create role use-scc-hostmount-anyuid --verb=use --resource=scc --resource-name=hostmount-anyuid -n $NAMESPACE_NFS
 oc adm policy add-role-to-user use-scc-hostmount-anyuid system:serviceaccount:$NAMESPACE_NFS:nfs-client-provisioner
 oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:$NAMESPACE_NFS:nfs-client-provisioner
-
-sleep 3
-
-echo "Applying manifests..."
-oc apply -f deploy/deployment.yaml
-oc apply -f deploy/storageclass.yaml
-
-#oc process -f template.yaml --param-file=env | oc create -f -
-
-#oc adm policy add-scc-to-user hostmount-anyuid -n ${NAMESPACE_NFS} -z nfs-client-provisioner
+oc adm policy add-scc-to-user hostmount-anyuid -n ${NAMESPACE_NFS} -z nfs-client-provisioner
 #oc adm policy add-scc-to-user hostmount-anyuid system:serviceaccount:$NAMESPACE_NFS:nfs-client-provisioner
+
+#sleep 3
+
+#oc apply -f deploy/deployment.yaml
+#oc apply -f deploy/storageclass.yaml
+
+
--- openshift-quickstarts/cluster-init/rh-reg-cluster-entitlement/run.sh
+++ openshift-quickstarts/cluster-init/rh-reg-cluster-entitlement/run.sh
@@ -5,5 +5,5 @@ PEM_FILE="./subscription.pem"
 echo "Templating manifest..."
 sed  "s/BASE64_ENCODED_PEM_FILE/$(base64 -w 0 ${PEM_FILE})/g" 0003-cluster-wide-machineconfigs.yaml.template > 0003-cluster-wide-machineconfigs.yaml
 
-echo "Creating secret..."
+echo "Creating MachineConfig..."
 oc apply -f 0003-cluster-wide-machineconfigs.yaml
