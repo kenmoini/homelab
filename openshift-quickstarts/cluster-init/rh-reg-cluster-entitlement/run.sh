161bf5d71b9e30ade4974c9cb34f8701fec85f01
Ken Moini <ken@kenmoini.com>
2021-09-18 13:14:43 -0400

    update rh entitlement

--- openshift-quickstarts/cluster-init/rh-reg-cluster-entitlement/run.sh
+++ openshift-quickstarts/cluster-init/rh-reg-cluster-entitlement/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PEM_FILE="./subscription.pem"
+PEM_FILE="$HOME/rh-entitlement.pem"
 
 echo "Templating manifest..."
 sed  "s/BASE64_ENCODED_PEM_FILE/$(base64 -w 0 ${PEM_FILE})/g" 0003-cluster-wide-machineconfigs.yaml.template > 0003-cluster-wide-machineconfigs.yaml
