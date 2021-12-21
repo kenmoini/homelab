89f16708a6220e505006c08bfd29083de15118d2
Ken Moini <ken@kenmoini.com>
2021-12-20 21:53:06 -0500

    add ntp container

---
+++ containers-as-a-service/caas-ntp/config/service_start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -x
+
+source /opt/service-containers/caas-ntp/config/service_vars.sh
+
+/opt/service-containers/caas-ntp/config/service_stop.sh
+
+sleep 3
+
+echo "Checking for stale network lock file..."
+FILE_CHECK="/var/lib/cni/networks/${NETWORK_NAME}/${IP_ADDRESS}"
+if [[ -f "$FILE_CHECK" ]]; then
+    rm $FILE_CHECK
+fi
+
+echo "Starting container ${CONTAINER_NAME}..."
+/usr/bin/podman run -d --rm --name "${CONTAINER_NAME}" \
+ --cap-add SYS_TIME \
+ --cap-add NET_BIND_SERVICE \
+ --network "${NETWORK_NAME}" \
+ --ip "${IP_ADDRESS}" \
+ ${CONTAINER_PORTS} \
+ ${RESOURCE_LIMITS} \
+ ${CONTAINER_SOURCE}
---
+++ containers-as-a-service/caas-ntp/config/service_vars.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CONTAINER_NAME="caas-ntp"
+NETWORK_NAME="lanBridge"
+IP_ADDRESS="192.168.42.14"
+
+CONTAINER_PORTS="-p 123/udp -p 321/udp"
+
+CONTAINER_SOURCE="quay.io/kenmoini/ubi8-ntp-server:latest"
+
+RESOURCE_LIMITS="-m 256m"
