23fb340bb36fa764fa3066bdfc235e5946525c10
Ken Moini <ken@kenmoini.com>
2022-12-22 00:22:00 -0500

    made that way more simple

--- bash_scripts/create_vm-vroom.sh
+++ bash_scripts/create_vm-vroom.sh
@@ -1,35 +1,23 @@
 #!/bin/bash
 
 ## Just change the VIRT_HOST, and run!
-VIRT_HOST="suki"
 VM_NAME="vroom"
-
-LOGF="${VM_NAME}.virt-install.log"
-echo "" > $LOGF
-
-RAZA_LIBVIRT_HOST="192.168.42.40"
-SUKI_LIBVIRT_HOST="192.168.42.46"
-
 BRIDGE_IFACE="bridge0"
 
-ISO_PATH="/mnt/fastAndLoose/nfs/isos/Windows10Pro64bit.iso"
-VIRT_IO_ISO_PATH="/mnt/fastAndLoose/nfs/isos/virtio-win.iso"
-VM_PATH="/mnt/fastAndLoose/nfs/vms/${VIRT_HOST}"
+ISO_PATH="/mnt/isos/Windows10Pro64bit.iso"
+VIRT_IO_ISO_PATH="/mnt/isos/virtio-win.iso"
+VM_PATH="/var/lib/libvirt/images/"
 
-VCPUS="sockets=1,cores=4,threads=1"
-RAM="16386"
+VCPUS="sockets=1,cores=6,threads=1"
+RAM="32772"
 
-MAC_PREFIX="54:52:00:42:07:"
 SUKI_RX_DEV="--hostdev=21:00.0,address.type=pci,address.multifunction=on --hostdev=21:00.1,address.type=pci,address.multifunction=on"
 
-LIKE_OPTIONS="-v --memballoon none --cpu host-passthrough --autostart --noautoconsole --virt-type kvm --features kvm_hidden=on --controller type=scsi,model=virtio-scsi --cdrom=${ISO_PATH} --cdrom=${VIRT_IO_ISO_PATH} --os-variant=win10 --os-type=windows --events on_reboot=restart --network bridge=${BRIDGE_IFACE},model=virtio"
+LIKE_OPTIONS="-v --memballoon none --cpu host-passthrough --autostart --noautoconsole --virt-type kvm --features kvm_hidden=on --controller type=scsi,model=virtio-scsi --cdrom=${ISO_PATH} --disk ${VIRT_IO_ISO_PATH},device=cdrom,bus=sata --os-variant=win10 --os-type=windows --events on_reboot=restart --network bridge=${BRIDGE_IFACE},model=virtio"
 
-## SUKI VMS
-if [[ $VIRT_HOST == "suki" ]]; then
-  # Detatch Devices from host :'(
-  virsh nodedev-detach pci_0000_21_00_0
-  virsh nodedev-detach pci_0000_21_00_1
-  
-  # Build VM
-  nohup virt-install --mac="${MAC_PREFIX}69" --name=${VIRT_HOST}-${VM_NAME} --vcpus ${VCPUS} --memory=${RAM} --disk size=600,path=${VM_PATH}/${VIRT_HOST}-${VM_NAME}.qcow2,cache=none ${SUKI_RX_DEV} --graphics vnc,listen=${SUKI_LIBVIRT_HOST},tlsport=,defaultMode='insecure' ${LIKE_OPTIONS} &>>$LOGF &
-fi
+# Detatch Devices from host :'(
+virsh nodedev-detach pci_0000_21_00_0
+virsh nodedev-detach pci_0000_21_00_1
+
+# Build VM
+virt-install --name=${VM_NAME} --vcpus ${VCPUS} --memory=${RAM} --disk size=250,path=${VM_PATH}/${VM_NAME}.qcow2,cache=none ${SUKI_RX_DEV} --graphics vnc,listen=0.0.0.0,tlsport=-1,defaultMode='insecure' ${LIKE_OPTIONS}
