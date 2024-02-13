34ea30ea80cacbc63ef3ae02647af3e9609fbf7c
Ken Moini <ken@kenmoini.com>
2024-02-13 14:39:59 -0500

    Update start.sh - add env vars

--- legacy/containers-as-a-service/squid-proxy/container_root/start.sh
+++ legacy/containers-as-a-service/squid-proxy/container_root/start.sh
@@ -2,9 +2,12 @@
 
 source /etc/sysconfig/squid
 
+SSL_DB_PATH="${SSL_DB_PATH:-"/etc/squid/certs"}"
+SSL_DB_SIZE="${SSL_DB_SIZE:-"64MB"}"
+
 /usr/libexec/squid/cache_swap.sh
 
-/usr/lib64/squid/security_file_certgen -c -s /etc/squid/certs/ssl_db -M 64MB
+/usr/lib64/squid/security_file_certgen -c -s ${SSL_DB_PATH}/ssl_db -M ${SSL_DB_SIZE}
 
 /usr/sbin/squid -k parse -f ${SQUID_CONF}
 
